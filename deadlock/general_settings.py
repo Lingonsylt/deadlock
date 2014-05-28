0331d11431c720ee7f953318c11b7ebed01f27e0
Anton Blomberg <anton@naetet.se>
2014-05-29 01:39:27 +0200

    Moved the SECRET_KEY from general_settings.py to avoid mistakenly not settings it in local_settings.py on server

--- deadlock/general_settings.py
+++ deadlock/general_settings.py
@@ -17,9 +17,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aw0=56in35vjo!i!#%&k9fcq@lg=(4^r74rd2^mdnvj&!2@1!='
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
