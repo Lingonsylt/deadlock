a7226dfa30cd845876cf93d2f171fc9a60d92d67
Anton Blomberg <anton@naetet.se>
2014-08-05 23:38:58 +0200

    Fixed urlpatterns so that invalid urls return 404 instead of the blog page

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from blog import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'p(?P<slug>[a-z0-9-]+)/', views.post, name="post"),
-    url(r'ckeditor/', include('ckeditor.urls')),
-    url(r'', views.blog, name="blog"),
+    url(r'^ckeditor/', include('ckeditor.urls')),
+    url(r'^(?P<slug>[a-z0-9-]+)/$', views.post, name="post"),
+    url(r'^$', views.blog, name="blog"),
 )
