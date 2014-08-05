7d511d2fffa7f056f2d48691651bfe3389516120
Anton Blomberg <anton@naetet.se>
2014-08-05 23:25:03 +0200

    Fixed urlpatterns so that invalid urls return 404 instead of the start page

--- deadlock/urls.py
+++ deadlock/urls.py
@@ -7,7 +7,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'', include('bootlog.urls')),
-    url(r'blog/', include('blog.urls')),
-    url(r'', blog.views.about, name="about"),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^$', blog.views.about, name="about"),
 )
